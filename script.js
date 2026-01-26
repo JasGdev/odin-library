068ddb6622aaf6081c33dc0e295d29de43e44485
Jason <jason13.deng@gmail.com>
2026-01-26 17:50:50 +0900

    Start layout of html and setting up display function

---
+++ script.js
@@ -0,0 +1,43 @@
+const myLibrary = [];
+const display = document.querySelector(".display");
+
+
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 310, true);
+addBookToLibrary("1984", "George Orwell", 328, false);
+addBookToLibrary("Clean Code", "Robert C. Martin", 464, true);
+addBookToLibrary("The Pragmatic Programmer", "Andrew Hunt & David Thomas", 352, false);
+
+const book = document.createElement('div')
+book.classList.add('book')
+book.textContent = "The Hobbit"
+display.appendChild(book)
+
+
+console.log(myLibrary)
+
+function Book(title, author, pages, read) {
+    if (!new.target) {
+        throw Error("You must use the 'new' operator to call the constructor");
+    }
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = crypto.randomUUID();
+    this.info = function () {
+        return this.read ? `${title} by ${author}, ${pages} pages, already read` :
+            `${title} by ${author}, ${pages} pages, not read yet`;
+    }
+}
+
+function addBookToLibrary(title, author, pages, read) {
+    // take params, create a book then store it in the array
+    let newBook = new Book(title, author, pages, read);
+    myLibrary.push(newBook);
+}
+
+function displayBooks(){
+    
+    
+}
+
